f8449ac978100098e6204df8c655edbcdf3e3e6a
VasilyKV <103107192+VasilyKV@users.noreply.github.com>
2022-07-08 08:44:56 +0300

    changed fields params of pokemon_entities

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -12,13 +12,13 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     lat = models.FloatField()
     lon = models.FloatField()
-    appeared_at = models.DateTimeField(default=None, null=True)
-    disappeared_at = models.DateTimeField(default=None, null=True)
-    level = models.IntegerField()
-    health = models.IntegerField()
-    strenght = models.IntegerField()
-    defence = models.IntegerField()
-    stamina = models.IntegerField()
+    appeared_at = models.DateTimeField(blank=True, default=None, null=True)
+    disappeared_at = models.DateTimeField(blank=True, default=None, null=True)
+    level = models.IntegerField(blank=True, null=True)
+    health = models.IntegerField(blank=True, null=True)
+    strenght = models.IntegerField(blank=True, null=True)
+    defence = models.IntegerField(blank=True, null=True)
+    stamina = models.IntegerField(blank=True, null=True)
     
     def __str__(self):
         return self.pokemon.title
